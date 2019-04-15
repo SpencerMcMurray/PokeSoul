88b10d7d886f258029ced772d3a62c848082e8af
mcmurr32 <spencermcmurray@yahoo.ca>
2019-04-15 15:12:50 -0400

    Removed basic create-react-app files

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Customers from './components/customers';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Express Starter</h1>
-        </header>
-        <Customers />
+          <h1>Hello there</h1>
       </div>
     );
   }
 }
-
-export default App;
