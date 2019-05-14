be290aa0a41bd3fc70d8bad6a8c5a52f331e3839
Spencer <spencermcmurray@yahoo.ca>
2019-05-14 15:05:42 -0400

    Fixed API fetch bug

--- src/components/editor/PokeDisplay.js
+++ src/components/editor/PokeDisplay.js
@@ -5,7 +5,7 @@ import ModalManager from "./ModalManager";
 const Pokedex = require("pokeapi-js-wrapper");
 const options = {
   protocol: "https",
-  hostName: "https://pokesoul.herokuapp.com",
+  hostName: "pokeapi.co",
   versionPath: "/api/v2/",
   cache: true,
   timeout: 5 * 1000 // 5s
